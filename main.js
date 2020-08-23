7f054caca170efa096d06ad8d691cabea5e9ed0e
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2020-08-24 00:21:17 +0530

    added mobile css

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var shareIcon = document.querySelector('.share-icon-background');
 var popup = document.querySelector('.action-state-popup');
+var contactArea = document.querySelector('.card-content-contact');
 
 shareIcon.addEventListener('click', function (event) {
    popup.classList.toggle("toggle-popup");
+   shareIcon.classList.toggle("share-icon-background-transition");
+   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 });
