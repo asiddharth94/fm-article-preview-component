c70942c02d0e779d39a35e932b711c02610fbe04
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2020-08-24 17:13:08 +0530

    added share action-state for mobile device

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 var shareIcon = document.querySelector('.share-icon-background');
 var popup = document.querySelector('.action-state-popup');
-var contactArea = document.querySelector('.card-content-contact');
+var popupTriangle = document.querySelector('.action-state-popup-triangle');
+var cardContactContent = document.querySelector('.card-content-contact');
+var avatar = document.querySelector('.avatar');
+var contactInfo = document.querySelector('.card-content-contact-info');
+// var mobileActionState = document.querySelector('.mobile-action-state-popup');
 
 shareIcon.addEventListener('click', function (event) {
-   popup.classList.toggle("toggle-popup");
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if(isMobile) {
+        popup.classList.add('mobile-action-state-popup');
+        shareIcon.classList.toggle('mobile-share-icon-action-state');
+        popupTriangle.classList.toggle("toggle-display");
+    }
+   popup.classList.toggle("toggle-display");
    shareIcon.classList.toggle("share-icon-background-transition");
-   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 });
